62bfd1e10958bc254830b7a302789e00e3fc7ec0
fermi_dirac <ManueleBryan@gmail.com>
2018-08-14 13:51:51 -0700

    instagram automation moved to its own file

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const puppeteer = require('puppeteer');
-
-const config = require('./config');
-const credentials = require('./credetials');
 const { appendRandomTags, saveImage, squareImage } = require('./utils');
+const config = require('./config');
 
+const instaAutomation = require('./instaAutomation');
 const { retrieveRedditPost } = require('./reddit-scraper');
 
-const USERNAME_SELECTOR = '[name="username"]';
-const PASSWORD_SELECTOR = '[name="password"]';
-const LOGIN_SELECTOR = 'form > span > button';
-
-const NOTNOW_SELECTOR = '[href="/"]';
-
-const NEWPOST_SELECTOR = `[aria-label="New Post"]`;
-const IMAGE_UPLOAD_SELECTOR = '[accept="image/jpeg"]';
-
-const GUIDE_LINE_SELECTOR = `[style="right: 33%; top: 0%; width: 1px; height: 100%;"]`;
-const HEADER_BUTTONS_SELECTOR = `header > div > div > button`;
-
-const CAPTION_SELECTOR = '[aria-label="Write a caption…"]';
-
+/** main */
 (async () => {
   try {
     const redditPost = await retrieveRedditPost();
@@ -29,67 +14,9 @@ const CAPTION_SELECTOR = '[aria-label="Write a caption…"]';
     await squareImage(fileName);
     const imagePath = __dirname + '/postimage.png';
 
-    await simulateBrowser({imagePath, caption: redditPost.topComment });
+    await instaAutomation.post({imagePath, caption: redditPost.topComment });
 
   } catch (error) {
     console.error(error);
   }
 })();
-
-async function simulateBrowser({ imagePath, caption }) {
-  try {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 20 });
-    const page = await browser.newPage();
-    page.setUserAgent(config.userAgent);
-
-    /* go to login page */
-    await page.goto('https://www.instagram.com/accounts/login/');
-
-    /* authenticate with username, password */
-    await page.waitFor(USERNAME_SELECTOR);
-    await page.waitFor(PASSWORD_SELECTOR);
-
-    await page.tap(USERNAME_SELECTOR);
-    await page.keyboard.type(credentials.username);
-
-    await page.tap(PASSWORD_SELECTOR);
-    await page.keyboard.type(credentials.password);
-
-    await page.tap(LOGIN_SELECTOR);
-
-    /* dismiss prompt to remember user */
-    await page.waitFor(NOTNOW_SELECTOR);
-    await page.tap(NOTNOW_SELECTOR);
-
-    /* post image */
-    await page.waitFor(NEWPOST_SELECTOR);
-
-    await page.tap(NEWPOST_SELECTOR);
-
-    const fileUploadInputs = await page.$$(IMAGE_UPLOAD_SELECTOR);
-
-    for (let i = 0; i < fileUploadInputs.length; ++i) {
-      let fileUploadInput = fileUploadInputs[i];
-
-      await fileUploadInput.uploadFile(imagePath);
-    }
-
-    /* confirm image editing */
-    await page.waitFor(GUIDE_LINE_SELECTOR);
-    let headerButtons = await page.$$(HEADER_BUTTONS_SELECTOR);
-    await headerButtons[1].tap();
-
-    /* adding comment */
-    await page.waitFor(CAPTION_SELECTOR);
-    await page.tap(CAPTION_SELECTOR);
-    await page.keyboard.type(caption);
-
-    headerButtons = await page.$$(HEADER_BUTTONS_SELECTOR);
-    await headerButtons[1].tap();
-
-    // await browser.close();
-
-  } catch (error) {
-    throw error;
-  }
-}
