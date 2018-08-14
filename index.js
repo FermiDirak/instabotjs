2eb3111fc21d935420ed12a9f8f9755f8e796b79
fermi_dirac <ManueleBryan@gmail.com>
2018-08-13 18:40:25 -0700

    post capability works

--- index.js
+++ index.js
@@ -12,15 +12,23 @@ const LOGIN_SELECTOR = 'form > span > button';
 
 const NOTNOW_SELECTOR = 'main > div > button';
 
-const NEWPOST_SELECTOR = '[aria-label="New Post"]';
+const NEWPOST_SELECTOR = `[aria-label="New Post"]`;
+const IMAGE_UPLOAD_SELECTOR = '[accept="image/jpeg"]';
+
+const GUIDE_LINE_SELECTOR = `[style="right: 33%; top: 0%; width: 1px; height: 100%;"]`;
+const HEADER_BUTTONS_SELECTOR = `header > div > div > button`;
+
+const CAPTION_SELECTOR = 'textarea';
 
 (async () => {
   try {
     const redditPost = await retrieveRedditPost();
     redditPost.topComment = appendRandomTags(redditPost.topComment);
-    const fileName = await saveImage(redditPost.imageUrl);
+    let fileName = await saveImage(redditPost.imageUrl);
 
-    console.log(__dirname + '/' + fileName);
+    fileName = __dirname + '/' + fileName;
+
+    simulateBrowser({fileName, caption: redditPost.topComment });
 
   } catch (error) {
     console.error(error);
@@ -28,14 +36,16 @@ const NEWPOST_SELECTOR = '[aria-label="New Post"]';
 
 })();
 
-async function simulateBrowser() {
+async function simulateBrowser({ fileName, caption }) {
   try {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     page.setUserAgent(config.userAgent);
-    // page.setViewport({ isMobile: true });
+
+    /* go to login page */
     await page.goto('https://www.instagram.com/accounts/login/');
 
+    /* authenticate with username, password */
     await page.waitFor(USERNAME_SELECTOR);
     await page.waitFor(PASSWORD_SELECTOR);
 
@@ -47,11 +57,35 @@ async function simulateBrowser() {
 
     await page.tap(LOGIN_SELECTOR);
 
+    /* dismiss prompt to remember user */
     await page.waitFor(NOTNOW_SELECTOR);
     await page.tap(NOTNOW_SELECTOR);
 
+    /* post image */
     await page.waitFor(NEWPOST_SELECTOR);
 
+    await page.tap(NEWPOST_SELECTOR);
+
+    const fileUploadInputs = await page.$$(IMAGE_UPLOAD_SELECTOR);
+
+    for (let i = 0; i < fileUploadInputs.length; ++i) {
+      let fileUploadInput = fileUploadInputs[i];
+
+      await fileUploadInput.uploadFile(fileName);
+    }
+
+    /* confirm image editing */
+    await page.waitFor(GUIDE_LINE_SELECTOR);
+    let headerButtons = await page.$$(HEADER_BUTTONS_SELECTOR);
+    await headerButtons[1].tap();
+
+    /* adding comment */
+    await page.waitFor(CAPTION_SELECTOR);
+    await page.tap(CAPTION_SELECTOR);
+    await page.keyboard.type(caption);
+
+    headerButtons = await page.$$(HEADER_BUTTONS_SELECTOR);
+    await headerButtons[1].tap();
 
     // await browser.close();
 
