6b02b2cfdc0ae4821699a1737d4978f5c5bf68d7
fermi_dirac <ManueleBryan@gmail.com>
2018-08-14 14:19:10 -0700

    documented functions

--- reddit-scraper.js
+++ reddit-scraper.js
@@ -3,6 +3,7 @@ const config = require('./config');
 
 const { imageTypes } = require('./constants');
 
+/** retrieves a random reddit post from the front page of subreddit */
 async function retrieveRedditPost() {
   try {
     const url = `https://www.reddit.com/r/${config.subreddit}/.json`;
@@ -14,10 +15,14 @@ async function retrieveRedditPost() {
     return redditPost;
 
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 }
 
+/** plucks a random post from list of posts
+ * @param {obj} posts Response representing top posts from subreddit
+ * @return {obj} instagram ready post data
+ */
 async function pluckRandomPost(posts) {
   posts = posts.sort((a, b) => Math.random() > 0.5 ? 1 : -1);
 
@@ -49,7 +54,9 @@ async function pluckRandomPost(posts) {
 
 }
 
-
+/** gets the top comment from the reddit post
+ * @param {string} a json representation of the post to retrieve a comment from
+ * @return {string} top comment */
 async function getTopComment(post) {
   try {
     const { permalink } = post.data;
@@ -61,7 +68,7 @@ async function getTopComment(post) {
     return comments[0].data.body;
 
   } catch(error) {
-    return error;
+    return post.title || error;
   }
 }
 
