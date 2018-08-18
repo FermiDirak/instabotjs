5ac61075b4abb909970169b667318a9e6377193b
fermi_dirac <ManueleBryan@gmail.com>
2018-08-18 00:28:59 -0700

    set up bash functionality

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env ts-node
+
 const { appendRandomTags, saveImage, squareImage } = require('./utils');
 const config = require('./config');
 const program = require('commander');
@@ -5,32 +7,42 @@ const program = require('commander');
 const instaAutomation = require('./instaAutomation');
 const { retrieveRedditPost } = require('./reddit-scraper');
 
-// program
-//   .version('1.0.0')
-//   .parse(process.argv);
+program
+  .version('1.0.0', '-v --version')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
 
-/** main */
-(async () => {
-  try {
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
 
-    const session = await instaAutomation.createSession();
-    await instaAutomation.login(session);
+// /** main */
+// (async () => {
+//   try {
 
-    while (true) {
-      const redditPost = await retrieveRedditPost();
-      redditPost.topComment = appendRandomTags(redditPost.topComment);
-      const fileName = await saveImage(redditPost.imageUrl);
+//     const session = await instaAutomation.createSession();
+//     await instaAutomation.login(session);
 
-      await squareImage(fileName);
-      const imagePath = __dirname + '/postimage.png';
+//     while (true) {
+//       const redditPost = await retrieveRedditPost();
+//       redditPost.topComment = appendRandomTags(redditPost.topComment);
+//       const fileName = await saveImage(redditPost.imageUrl);
 
-      console.log(redditPost);
+//       await squareImage(fileName);
+//       const imagePath = __dirname + '/postimage.png';
 
-      await instaAutomation.post(session, {imagePath, caption: redditPost.topComment });
-      await instaAutomation.followAll(session);
-    }
+//       console.log(redditPost);
 
-  } catch (error) {
-    console.error(error);
-  }
-})();
+//       await instaAutomation.post(session, {imagePath, caption: redditPost.topComment });
+//       await instaAutomation.followAll(session);
+//     }
+
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
